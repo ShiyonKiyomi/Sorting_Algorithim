71cc6eed2c9208f4fa1fcb1e5e0be8a76bdbdb4c
DJPATZZ <urbinapatrick10@gmail.com>
2026-09-01 20:04:31 +0800

    Add merge sort and quick sort

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ class Main{
 
         //Getting user input
         algoSelect = Helper.getValidChoice(myScanner,
-                "Select Algorithm:\n[1]Insertion sort\n[2]Selection sort\nSelect: ", 1, 2);
+                "Select Algorithm:\n[1]Insertion sort\n[2]Selection sort\n[3]Merge sort\n[4]Quick sort\nSelect: ", 1, 4);
 
         dataSelect = Helper.getValidChoice(myScanner,
                 "\nSelect DataType input:\n[1]Integer\n[2]String\nSelect: ", 1, 2);
@@ -20,8 +20,12 @@ class Main{
             Helper.fillArray(myScanner, numbersArray, Scanner::nextInt);
             if(algoSelect == 1){
                 Sorting.insertionSort(numbersArray);
-            } else{
+            } else if (algoSelect == 2){
                 Sorting.selectionSort(numbersArray);
+            } else if (algoSelect == 3){
+                Sorting.mergeSort(numbersArray);
+            } else {
+                Sorting.quickSort(numbersArray);
             }
             System.out.println("Sorted elements: " + java.util.Arrays.toString(numbersArray));
 
@@ -30,8 +34,12 @@ class Main{
             Helper.fillArray(myScanner, stringArray, Scanner::next);
             if (algoSelect == 1){
                 Sorting.insertionSort(stringArray);
-            } else{
+            } else if (algoSelect == 2){
                 Sorting.selectionSort(stringArray);
+            } else if (algoSelect == 3){
+                Sorting.mergeSort(stringArray);
+            } else {
+                Sorting.quickSort(stringArray);
             }
             System.out.println("Sorted elements: " + java.util.Arrays.toString(stringArray));
 
--- src/Sorting.java
+++ src/Sorting.java
@@ -38,4 +38,92 @@ public class Sorting {
             Helper.printState(array, i , min_idx);
         }
     }
+
+    //merge sort
+    public static <T extends Comparable<T>> void mergeSort(T[] array){
+        if (array.length < 2) {
+            return;
+        }
+
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] array, int left, int right){
+        if (left >= right) {
+            return;
+        }
+
+        int middle = left + (right - left) / 2;
+        mergeSort(array, left, middle);
+        mergeSort(array, middle + 1, right);
+        merge(array, left, middle, right);
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] array, int left, int middle, int right){
+        T[] merged = Arrays.copyOfRange(array, left, right + 1);
+        int leftIndex = 0;
+        int rightIndex = middle - left + 1;
+        int outputIndex = left;
+
+        while (leftIndex <= middle - left && rightIndex <= right - left) {
+            if (merged[leftIndex].compareTo(merged[rightIndex]) <= 0) {
+                array[outputIndex] = merged[leftIndex++];
+            } else {
+                array[outputIndex] = merged[rightIndex++];
+            }
+            Helper.printState(array, outputIndex, outputIndex);
+            outputIndex++;
+        }
+
+        while (leftIndex <= middle - left) {
+            array[outputIndex] = merged[leftIndex++];
+            Helper.printState(array, outputIndex, outputIndex);
+            outputIndex++;
+        }
+
+        while (rightIndex <= right - left) {
+            array[outputIndex] = merged[rightIndex++];
+            Helper.printState(array, outputIndex, outputIndex);
+            outputIndex++;
+        }
+    }
+
+    //quick sort
+    public static <T extends Comparable<T>> void quickSort(T[] array){
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high){
+        if (low >= high) {
+            return;
+        }
+
+        int pivotIndex = partition(array, low, high);
+        quickSort(array, low, pivotIndex - 1);
+        quickSort(array, pivotIndex + 1, high);
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] array, int low, int high){
+        T pivot = array[high];
+        int smallerIndex = low;
+
+        for (int currentIndex = low; currentIndex < high; currentIndex++) {
+            Helper.printState(array, currentIndex, high);
+            if (array[currentIndex].compareTo(pivot) <= 0) {
+                swap(array, smallerIndex, currentIndex);
+                Helper.printState(array, smallerIndex, currentIndex);
+                smallerIndex++;
+            }
+        }
+
+        swap(array, smallerIndex, high);
+        Helper.printState(array, smallerIndex, high);
+        return smallerIndex;
+    }
+
+    private static <T> void swap(T[] array, int first, int second){
+        T temporary = array[first];
+        array[first] = array[second];
+        array[second] = temporary;
+    }
 }
