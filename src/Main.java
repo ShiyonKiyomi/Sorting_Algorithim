919aca23322f811b7530dd0d90941fb89dc32c56
Shiyon <helllight39@gmail.com>
2026-09-02 17:39:19 +0800

    msg

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,60 @@
 import java.util.*;
 
 class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
         int algoSelect;
         int dataSelect;
         int orderSelect;
+        boolean running = true;
 
         //Getting user input
-        algoSelect = Helper.getValidChoice(myScanner,
-                "Select Algorithm:\n[1]Insertion sort\n[2]Selection sort\n[3]Merge sort\n[4]Quick sort\nSelect: ", 1, 4);
+        while (running) {
+            printMenu();
+            int selection = myScanner.nextInt();
 
-        dataSelect = Helper.getValidChoice(myScanner,
-                "\nSelect DataType input:\n[1]Integer\n[2]String\n[3]Double\n[4]Character\nSelect: ", 1, 4);
+            if (selection == 0) {
+                running = false;
+                continue;
+            }
 
-        orderSelect = Helper.getValidChoice(myScanner,
-                "\nSelect Sorting Order:\n[1]Ascending\n[2]Descending\nSelect: ", 1, 2);
-        boolean ascending = orderSelect == 1;
+            algoSelect = Helper.getValidChoice(myScanner,
+                    "Select Algorithm:\n[1]Insertion sort\n[2]Selection sort\n[3]Merge sort\n[4]Quick sort\nSelect: ", 1, 4);
 
-        //choosing logic
-        if (dataSelect == 1) {
-            Integer[] numbersArray = new Integer[6];
-            Helper.fillArray(myScanner, numbersArray, Scanner::nextInt);
-            sortAndPrint(numbersArray, algoSelect, ascending);
-        } else if (dataSelect == 2) {
-            String[] stringArray = new String[6];
-            Helper.fillArray(myScanner, stringArray, Scanner::next);
-            sortAndPrint(stringArray, algoSelect, ascending);
-        } else if (dataSelect == 3) {
-            Double[] decimalArray = new Double[6];
-            Helper.fillArray(myScanner, decimalArray, Scanner::nextDouble);
-            sortAndPrint(decimalArray, algoSelect, ascending);
-        } else {
-            Character[] characterArray = new Character[6];
-            Helper.fillArray(myScanner, characterArray,
-                    scanner -> scanner.next().charAt(0));
-            sortAndPrint(characterArray, algoSelect, ascending);
+            dataSelect = Helper.getValidChoice(myScanner,
+                    "\nSelect DataType input:\n[1]Integer\n[2]String\n[3]Double\n[4]Character\nSelect: ", 1, 4);
+
+            orderSelect = Helper.getValidChoice(myScanner,
+                    "\nSelect Sorting Order:\n[1]Ascending\n[2]Descending\nSelect: ", 1, 2);
+            boolean ascending = orderSelect == 1;
+
+            //choosing logic
+            if (dataSelect == 1) {
+                Integer[] numbersArray = new Integer[6];
+                Helper.fillArray(myScanner, numbersArray, Scanner::nextInt);
+                sortAndPrint(numbersArray, algoSelect, ascending);
+            } else if (dataSelect == 2) {
+                String[] stringArray = new String[6];
+                Helper.fillArray(myScanner, stringArray, Scanner::next);
+                sortAndPrint(stringArray, algoSelect, ascending);
+            } else if (dataSelect == 3) {
+                Double[] decimalArray = new Double[6];
+                Helper.fillArray(myScanner, decimalArray, Scanner::nextDouble);
+                sortAndPrint(decimalArray, algoSelect, ascending);
+            } else {
+                Character[] characterArray = new Character[6];
+                Helper.fillArray(myScanner, characterArray,
+                        scanner -> scanner.next().charAt(0));
+                sortAndPrint(characterArray, algoSelect, ascending);
+            }
+
         }
-    }
+
+
+
+        }
+
 
     private static <T extends Comparable<T>> void sortAndPrint(T[] array, int algoSelect, boolean ascending) {
         switch (algoSelect) {
@@ -50,5 +66,16 @@ class Main{
         }
 
         System.out.println((ascending ? "Sorted ascending: " : "Sorted descending: ") + Arrays.toString(array));
+    }
+    public static void println(String args) {
+        System.out.print(args);
+    }
+    public static void printMenu(){
+        println("""
+                ---------- MENU --------------
+                1. Select Algorithim
+                0. Exit
+                Selection: 
+                """);
     }
 }
