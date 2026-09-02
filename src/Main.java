c5354ec8852a0b9532eb48f965b7806641077f0a
Patt-0 <urbinapatrickm@gmail.com>
2026-09-02 07:55:57 +0000

    dwdawda

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ class Main{
 
         int algoSelect;
         int dataSelect;
+        int orderSelect;
 
         //Getting user input
         algoSelect = Helper.getValidChoice(myScanner,
@@ -14,36 +15,40 @@ class Main{
         dataSelect = Helper.getValidChoice(myScanner,
                 "\nSelect DataType input:\n[1]Integer\n[2]String\n[3]Double\n[4]Character\nSelect: ", 1, 4);
 
+        orderSelect = Helper.getValidChoice(myScanner,
+                "\nSelect Sorting Order:\n[1]Ascending\n[2]Descending\nSelect: ", 1, 2);
+        boolean ascending = orderSelect == 1;
+
         //choosing logic
         if (dataSelect == 1) {
             Integer[] numbersArray = new Integer[6];
             Helper.fillArray(myScanner, numbersArray, Scanner::nextInt);
-            sortAndPrint(numbersArray, algoSelect);
+            sortAndPrint(numbersArray, algoSelect, ascending);
         } else if (dataSelect == 2) {
             String[] stringArray = new String[6];
             Helper.fillArray(myScanner, stringArray, Scanner::next);
-            sortAndPrint(stringArray, algoSelect);
+            sortAndPrint(stringArray, algoSelect, ascending);
         } else if (dataSelect == 3) {
             Double[] decimalArray = new Double[6];
             Helper.fillArray(myScanner, decimalArray, Scanner::nextDouble);
-            sortAndPrint(decimalArray, algoSelect);
+            sortAndPrint(decimalArray, algoSelect, ascending);
         } else {
             Character[] characterArray = new Character[6];
             Helper.fillArray(myScanner, characterArray,
                     scanner -> scanner.next().charAt(0));
-            sortAndPrint(characterArray, algoSelect);
+            sortAndPrint(characterArray, algoSelect, ascending);
         }
     }
 
-    private static <T extends Comparable<T>> void sortAndPrint(T[] array, int algoSelect) {
+    private static <T extends Comparable<T>> void sortAndPrint(T[] array, int algoSelect, boolean ascending) {
         switch (algoSelect) {
-            case 1 -> Sorting.insertionSort(array);
-            case 2 -> Sorting.selectionSort(array);
-            case 3 -> Sorting.mergeSort(array);
-            case 4 -> Sorting.quickSort(array);
+            case 1 -> Sorting.insertionSort(array, ascending);
+            case 2 -> Sorting.selectionSort(array, ascending);
+            case 3 -> Sorting.mergeSort(array, ascending);
+            case 4 -> Sorting.quickSort(array, ascending);
             default -> throw new IllegalArgumentException("Unknown sorting algorithm");
         }
 
-        System.out.println("Sorted elements: " + Arrays.toString(array));
+        System.out.println((ascending ? "Sorted ascending: " : "Sorted descending: ") + Arrays.toString(array));
     }
 }
--- src/Sorting.java
+++ src/Sorting.java
@@ -4,11 +4,15 @@ import java.util.function.*;
 public class Sorting {
     //insertion sort
     public static <T extends Comparable<T>> void insertionSort(T[] array){
+        insertionSort(array, true);
+    }
+
+    public static <T extends Comparable<T>> void insertionSort(T[] array, boolean ascending){
         for (int i =1; i < array.length ; i++){
             T key = array[i];
             int j = i - 1;
 
-            while(j >= 0 && array[j].compareTo(key) > 0){
+            while(j >= 0 && compare(array[j], key, ascending) > 0){
                 array[j+1] = array[j];
                     j--;
                     Helper.printState(array, j + 1 , i);
@@ -20,12 +24,16 @@ public class Sorting {
 
     //selection sort
     public static <T extends Comparable<T>> void selectionSort(T[] array){
+        selectionSort(array, true);
+    }
+
+    public static <T extends Comparable<T>> void selectionSort(T[] array, boolean ascending){
         for(int i = 0 ; i < array.length ; i ++){
             int min_idx = i;
 
             for (int j = i + 1 ; j < array.length ; j ++ ){
                 Helper.printState(array, min_idx, j);
-                if(array[j].compareTo(array[min_idx]) < 0 ){
+                if(compare(array[j], array[min_idx], ascending) < 0 ){
                     min_idx = j;
                 }
             }
@@ -41,32 +49,44 @@ public class Sorting {
 
     //merge sort
     public static <T extends Comparable<T>> void mergeSort(T[] array){
+        mergeSort(array, true);
+    }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] array, boolean ascending){
         if (array.length < 2) {
             return;
         }
 
-        mergeSort(array, 0, array.length - 1);
+        mergeSort(array, 0, array.length - 1, ascending);
     }
 
     private static <T extends Comparable<T>> void mergeSort(T[] array, int left, int right){
+        mergeSort(array, left, right, true);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] array, int left, int right, boolean ascending){
         if (left >= right) {
             return;
         }
 
         int middle = left + (right - left) / 2;
-        mergeSort(array, left, middle);
-        mergeSort(array, middle + 1, right);
-        merge(array, left, middle, right);
+        mergeSort(array, left, middle, ascending);
+        mergeSort(array, middle + 1, right, ascending);
+        merge(array, left, middle, right, ascending);
     }
 
     private static <T extends Comparable<T>> void merge(T[] array, int left, int middle, int right){
+        merge(array, left, middle, right, true);
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] array, int left, int middle, int right, boolean ascending){
         T[] merged = Arrays.copyOfRange(array, left, right + 1);
         int leftIndex = 0;
         int rightIndex = middle - left + 1;
         int outputIndex = left;
 
         while (leftIndex <= middle - left && rightIndex <= right - left) {
-            if (merged[leftIndex].compareTo(merged[rightIndex]) <= 0) {
+            if (compare(merged[leftIndex], merged[rightIndex], ascending) <= 0) {
                 array[outputIndex] = merged[leftIndex++];
             } else {
                 array[outputIndex] = merged[rightIndex++];
@@ -90,26 +110,38 @@ public class Sorting {
 
     //quick sort
     public static <T extends Comparable<T>> void quickSort(T[] array){
-        quickSort(array, 0, array.length - 1);
+        quickSort(array, true);
+    }
+
+    public static <T extends Comparable<T>> void quickSort(T[] array, boolean ascending){
+        quickSort(array, 0, array.length - 1, ascending);
     }
 
     private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high){
+        quickSort(array, low, high, true);
+    }
+
+    private static <T extends Comparable<T>> void quickSort(T[] array, int low, int high, boolean ascending){
         if (low >= high) {
             return;
         }
 
-        int pivotIndex = partition(array, low, high);
-        quickSort(array, low, pivotIndex - 1);
-        quickSort(array, pivotIndex + 1, high);
+        int pivotIndex = partition(array, low, high, ascending);
+        quickSort(array, low, pivotIndex - 1, ascending);
+        quickSort(array, pivotIndex + 1, high, ascending);
     }
 
     private static <T extends Comparable<T>> int partition(T[] array, int low, int high){
+        return partition(array, low, high, true);
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] array, int low, int high, boolean ascending){
         T pivot = array[high];
         int smallerIndex = low;
 
         for (int currentIndex = low; currentIndex < high; currentIndex++) {
             Helper.printState(array, currentIndex, high);
-            if (array[currentIndex].compareTo(pivot) <= 0) {
+            if (compare(array[currentIndex], pivot, ascending) <= 0) {
                 swap(array, smallerIndex, currentIndex);
                 Helper.printState(array, smallerIndex, currentIndex);
                 smallerIndex++;
@@ -125,5 +157,10 @@ public class Sorting {
         T temporary = array[first];
         array[first] = array[second];
         array[second] = temporary;
+    }
+
+    private static <T extends Comparable<T>> int compare(T first, T second, boolean ascending) {
+        int result = first.compareTo(second);
+        return ascending ? result : -result;
     }
 }
